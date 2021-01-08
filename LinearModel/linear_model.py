a4bbdcf0e713d7e703e505064545ce533fcd8a0b
zlp <liupeng.zhuang@cyclone-robotics.com>
2021-01-08 18:50:56 +0800

    linear model

---
+++ LinearModel/linear_model.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+
+
+class Logit:
+    def __init__(self, X, Y):
+        self.X = X
+        W = np.random.rand(X.shape[1]+1)
+        self.W = W[:-1]
+        self.b = W[-1]
+        self.Y = Y
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def get_z(self):
+        return np.dot(self.X, self.W.T) + self.b
+
+    def test(self):
+        print("x:\n", self.X)
+        print("w:\n", self.W)
+        # print(self.b)
+        print("\n", self.linear_func())
+
+
+
+class LDA:
+    def __init__(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    a = np.random.rand(3, 5)
+    a = a.T
+    logistic = Logit(a, [])
+    logistic.test()
