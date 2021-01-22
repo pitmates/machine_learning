f7848f58062c5bf057a0552b9de9e1636374703b
zlp <liupeng.zhuang@cyclone-robotics.com>
2021-01-22 19:48:58 +0800

    .

--- LinearModel/test.py
+++ LinearModel/test.py
@@ -1,4 +1,17 @@
 import numpy as np
+import pandas as pd
 import os
 
+scriptpath = os.path.abspath(__file__)
+rootpath = os.path.dirname(os.path.dirname(scriptpath))
 
+datapath = os.path.join(rootpath, 'data/LinearModel/watermelon3_0_Ch.csv')
+
+
+def testLogistic():
+    data = pd.read_csv(datapath)
+    print(data.values)
+
+
+if __name__ == "__main__":
+    testLogistic()
