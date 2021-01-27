e0e5e16d089821020436918c44c7a07439177880
zlp <liupeng.zhuang@cyclone-robotics.com>
2021-01-27 17:43:42 +0800

    deal data

--- LinearModel/test.py
+++ LinearModel/test.py
@@ -9,8 +9,18 @@ datapath = os.path.join(rootpath, 'data/LinearModel/watermelon3_0_Ch.csv')
 
 
 def testLogistic():
-    data = pd.read_csv(datapath)
-    print(data.values)
+    data = pd.read_csv(datapath).values
+
+    X = data[:, 7:9].astype(float)
+    y = data[:, 9]
+
+    y[y == '是'] = 1
+    y[y == '否'] = 0
+    y = y.astype(int)
+    
+    print(X)
+    print(y)
+    
 
 
 if __name__ == "__main__":
